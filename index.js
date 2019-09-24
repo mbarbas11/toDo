ea18c28729a666b7e1ceb945c4db26d1d9c626dc
Michael Barbas <michaelbarbas@gmail.com>
2019-09-23 21:23:59 -0400

    Handling requests

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 
+//setup
 const app = express();
 
+//routehandler url localhost:2000/....logged message waiting for resp
+app.get('/api',function(req, resp){
+    console.log('hello');
+    resp.send({ user: 'michael', id: 'mbarb009'});//resp.end(); //ends resp 
+});
+
 app.listen(2000, function(){
-    console.log('ready for req.')
+    console.log('ready for req.');
 });
