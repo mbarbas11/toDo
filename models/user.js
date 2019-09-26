1730dd3fd55ec93d070fbea778349ce0f8a1c274
Michael Barbas <michaelbarbas@gmail.com>
2019-09-26 14:38:30 -0400

    Created schema for geolocation

--- models/user.js
+++ models/user.js
@@ -2,6 +2,19 @@ const mongoose = require ('mongoose');
 const Schema = mongoose.Schema;
 
 //creating schema and model for user
+
+//geo_loc schema
+const geoSchema = new Schema({
+    type: {
+        type: String,
+        default: "Point"
+    },
+    coordinates: {
+        type: [Number],
+        index: "2dsphere" //type of map
+    }
+});
+
 const userSchema = new Schema({
     name:{
         type: String,
@@ -13,8 +26,9 @@ const userSchema = new Schema({
     available:{
         type : Boolean,
         default: false
-    }
-
+    },
+    geometry: geoSchema
+    
 });
 
 const User = mongoose.model('user', userSchema) //structured based on schema
